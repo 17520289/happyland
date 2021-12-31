3e87b0fecf2dbf994733b61ef53a6630c211a052
chautv <chautv@hlsolutions.jp>
2021-12-31 03:41:37 +0000

    Add assessment

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -12,7 +12,7 @@ Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
     'middleware' => array_merge(
         (array) config('backpack.base.web_middleware', 'web'),
-        
+        (array) backpack_middleware(),
     ),
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
